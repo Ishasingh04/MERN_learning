8390d79f699d2f826cd003e5b3e0c8a0229fb56e
Isha <54667057+Ishasingh04@users.noreply.github.com>
2020-09-19 03:56:10 +0530

    issue resolved and JWT implementation

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 const User = require('../../models/User');
 
@@ -28,7 +30,7 @@ router.post('/',[
             let user = await User.findOne({ email });
 
             if (user) {
-                res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+               return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
             }
 
             const avatar = gravatar.url(email,{
@@ -48,12 +50,21 @@ router.post('/',[
 
             user.password = await bcrypt.hash(password, salt);
 
-            await user,save();
+            await user.save();
             //Get users gravatar
             //Encrypt password
-            //Return jsonwebtoken
-            res.send('User registered');
-
+           const payload = {
+               user: {
+                   id: user.id
+               }
+           }
+           jwt.sign(
+               payload,
+               config.get('jwtSecret'),{expiresIn: 360000},
+               (err, token) => {
+                if(err) throw err;
+                res.json({token});
+            });
         } catch (err) {
             console.error(err.message);
             res.status(500).send('Server error');
