b78065760143cc54662d5d5e4d5933d6034b542c
Isha <54667057+Ishasingh04@users.noreply.github.com>
2020-09-21 17:53:03 +0530

    Add & Delete profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -86,7 +86,7 @@ router.post(
         if (instagram) profileFields.social.instagram = instagram;
 
 
-        console.log(profileFields.skills);
+        //console.log(profileFields.skills);
 
         try{
             let profile = await Profile.findOne({ user: req.user.id});
@@ -170,6 +170,84 @@ router.delete('/', auth,  async (req, res) => {
         res.status(500).send('Server Error');
     }
 });
+
+//@route  PUT api/profile/experience
+//@desc  ADD profile experience
+//@access Private
+router.put('/experience', [auth, [
+    check('title','Title is rquired')
+    .not()
+    .isEmpty(),
+    check('company','Company is rquired')
+    .not()
+    .isEmpty(),
+    check('from','From date is rquired')
+    .not()
+    .isEmpty(),
+    ]
+],
+async (req,res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors : errors.array()});
+    }
+    
+    const {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    } = req.body;
+
+    const newExp = {
+         title,
+         company,
+         location,
+         from,
+         to,
+        current,
+        description
+    }
+    try {
+        const profile = await Profile.findOne({user: req.user.id });
+
+        profile.experience.unshift(newExp);
+        await profile.save();
+
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+        
+    }
+});
+
+
+//@route  DELETE api/profile/experience/:exp_id
+//@desc  deleting profile experience
+//@access Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+        try {
+            const profile = await Profile.findOne({user: req.user.id });
+
+            //Get remove index
+            const removeIndex = profile.experience
+            .map(item => item.id)
+            .indexOf(req.params.exp_id);
+        profile.experience.splice(removeIndex, 1);
+
+        await profile.save();
+
+        res.json(profile);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+    });
+
 
 
 module.exports = router;
