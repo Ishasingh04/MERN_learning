5ef10eb1677a865daeab9f5ea43c40344640b075
Isha <54667057+Ishasingh04@users.noreply.github.com>
2020-09-30 22:28:10 +0530

    console error

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useState } from 'react';
+import axios from 'axios';
 
 const Register = () => {
 const [formData, setFormData ] = useState({
@@ -13,12 +14,30 @@ const { name, email, password, password2 } = formData;
 const onChange = e => 
 setFormData({ ...formData, [e.target.name]: e.target.value});
 
-const onSubmit = e => {
+const onSubmit = async e => {
     e.preventDefaut();
     if(password !== password2) {
         console.log('Password do not match');
     } else{
-        console.log(formData);
+        const newUser = {
+          name,
+          email,
+          password
+        }
+        try {
+          const config = {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          }
+
+          const body = JSON.stringify(newUser);
+
+          const res = await axios.post('/api/users', body,config)
+          console.log(res.data);
+        } catch (err) {
+          console.error(err.response.data);
+        }
     }
 };
 
