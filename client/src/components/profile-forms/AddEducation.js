34fddd3b2fb43937e9eae20a0da09d22c5dea544
Isha <54667057+Ishasingh04@users.noreply.github.com>
2020-10-13 17:21:36 +0530

    Add Experience

---
+++ client/src/components/profile-forms/AddEducation.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+const AddEducation = props => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+AddEducation.propTypes = {
+
+}
+export default AddEducation;
