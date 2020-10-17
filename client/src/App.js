86bf1d1845a542a7daf6cecfbe1f748c15c91d96
Isha <54667057+Ishasingh04@users.noreply.github.com>
2020-10-17 23:00:50 +0530

    Getting started on Profile

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import PrivateRoute from './components/routing/PrivateRoute';
 import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
 //Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -38,9 +39,10 @@ return (
     <section className = "container">
      <Alert />
       <Switch>
-      <Route exact path="/register" component={Register } />
-      <Route exact path="/login" component={Login } />
-      <Route exact path="/profiles" component={Profiles } />
+      <Route exact path="/register" component={ Register } />
+      <Route exact path="/login" component={ Login } />
+      <Route exact path="/profiles" component={ Profiles } />
+      <Route exact path="/profile/:id" component={ Profile } />
       <PrivateRoute 
       exact
        path="/dashboard" 
